e1aee6701809f00a6359fb269cff4ac43f590f71
joordy <jmfronik@gmail.com>
2021-06-09 11:10:32 +0200

    fix cors config

--- cors/index.js
+++ cors/index.js
@@ -2,7 +2,10 @@ const cors = require('cors');
 
 const corsOptions = {
   // origin: 'http://localhost:3000',
-  origin: 'https://europcar.netlify.app',
+  // origin: 'https://europcar.netlify.app',
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
   optionSuccessStatus: 200,
 };
 
