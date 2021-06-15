b6501077f6983497c9bc5f5ce1bac97876746e93
joordy <jmfronik@gmail.com>
2021-06-15 16:48:05 +0200

    deployed latest

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const sgMail = require('@sendgrid/mail');
+const moment = require('moment');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const stripe = require('stripe')(
   'sk_test_51IsTukJEAzd2OWuLk3FnSrJQnDxX3VuWZRtUIkCCvEBhK20GOantGHhar8kn1eqtYLtZ1qSX0hvVZ2lwyRWkCl5n002JbZmNr2'
@@ -16,9 +17,6 @@ const stripe = require('stripe')(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-let carIndexKey;
-let reservation;
 
 // Get routes
 app.get('/api', getApi);
@@ -32,6 +30,8 @@ app.get('/api/deposit', getDeposit);
 app.post('/api/reservations', postReservation);
 app.post('/api/order-details', postOrderDetails);
 app.post('/api/carIndexKey', postCarIndexKey);
+app.post('/api/testEnv', postTestEnv);
+app.post('/api/payMethod', postPayMethod);
 app.post('/api/create-verification-session', postCreateVerificationSession);
 app.post('/api/create-checkin', postCreateCheckin);
 app.post('/api/create-payment-intent', postCreatePaymentIntent);
@@ -63,50 +63,52 @@ async function getReservation(req, res) {
 
 function getCarIndexKey(req, res) {
   // Receive chosen car index key
-  console.log('carKeyIndex:', carIndexKey);
-  res.json(carIndexKey);
+  setTimeout(async () => {
+    const cityRef = db.collection('server').doc('carIndexKey1');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      res.json(doc.data());
+    }
+  }, 100);
 }
 
 function getOrderDetails(req, res) {
   // Receives selected car obj from signed in user
-  console.log('current reservation', reservation);
-  const data = () => {
-    if (!reservation) {
-      console.log('not found');
+  setTimeout(async () => {
+    const cityRef = db.collection('server').doc('reservation');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
     } else {
-      return reservation;
+      res.json(doc.data());
     }
-  };
-
-  res.json(data());
+  }, 100);
 }
 
 function getVerification(req, res) {
-  //
-  console.log(reservation);
-  const data = () => {
-    if (!reservation) {
-      return 'undefined';
+  setTimeout(async () => {
+    const cityRef = db.collection('server').doc('reservation');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
     } else {
-      return reservation;
+      res.json(doc.data());
     }
-  };
-
-  res.send(data());
+  }, 100);
 }
 
 function getDeposit(req, res) {
-  // Get information
-  console.log(reservation);
-  const data = () => {
-    if (!reservation) {
-      return 'undefined';
+  setTimeout(async () => {
+    const cityRef = db.collection('server').doc('reservation');
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
     } else {
-      return reservation;
+      res.json(doc.data());
     }
-  };
-
-  res.send(data());
+  }, 100);
 }
 
 // //////////////////////////////////////
@@ -116,75 +118,118 @@ function getDeposit(req, res) {
 async function postReservation(req, res) {
   await db.collection('server').doc('loggedInUser').delete();
   await db.collection('server').doc('loggedInUser').set(req.body);
-
-  // await db.collection('server').doc('loggedInUser').set(req.body);
-  // const repa = await serverRef.set((loggedInUser = 'pietje'));
-
-  // console.log(serverRef);
-  // loggedInUser = req.body;
-  console.log('DIT  IS DE JUISTE DAAAATA', req.body);
+  console.log('DIT  IS DE JUISTE DAAAATA', req.body.firstName);
   console.log('hi');
-  // console.log('loggedInUser', loggedInUser);
-  // res.json(loggedInUser);
   res.send('success');
 }
 
-function postOrderDetails(req, res) {
+async function postOrderDetails(req, res) {
+  await db.collection('server').doc('reservation').delete();
+  await db.collection('server').doc('reservation').set(req.body);
+  console.log('post this detail', req.body);
   // Post specific car obj to server, to fetch on later.
-  reservation = req.body;
-  console.log('this is the one', req.body);
-  res.json(req.body);
+  // console.log('this is the one', req.body);
+  res.json('success');
 }
 
-function postCarIndexKey(req, res) {
+async function postCarIndexKey(req, res) {
   // Save chosen car index key on server
-  console.log('this is the key index', req.body);
-  carIndexKey = req.body;
-  res.json(req.body);
+  await db.collection('server').doc('carKeyIndex').delete();
+  await db.collection('server').doc('carKeyIndex ').set(req.body);
+  res.json('success');
+}
+
+async function postTestEnv(req, res) {
+  res.send('order details succesfully submitted');
+}
+
+async function postPayMethod(req, res) {
+  if (req.body.carkey === 0) {
+    await db
+      .collection('server')
+      .doc('loggedInUser')
+      .update({ 'carResOne.paidDeposit.paid': req.body.paid, 'carResOne.paidDeposit.method': req.body.method });
+    await db
+      .collection('server')
+      .doc('reservation')
+      .update({ paidDeposit: { paid: req.body.paid, method: req.body.method } });
+  } else if (req.body.carkey === 1) {
+    await db
+      .collection('server')
+      .doc('loggedInUser')
+      .update({ 'carResTwo.paidDeposit.paid': req.body.paid, 'carResTwo.paidDeposit.method': req.body.method });
+    await db
+      .collection('server')
+      .doc('reservation')
+      .update({ paidDeposit: { paid: req.body.paid, method: req.body.method } });
+  } else if (req.body.carkey === 3) {
+    await db
+      .collection('server')
+      .doc('loggedInUser')
+      .update({ 'carResThree.paidDeposit.paid': req.body.paid, 'carResThree.paidDeposit.method': req.body.method });
+    await db
+      .collection('server')
+      .doc('reservation')
+      .update({ paidDeposit: { paid: req.body.paid, method: req.body.method } });
+  }
+  res.json('success');
+}
+
+async function postData(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: {
+      Authorization: `Basic ${process.env.WALLET_SECRET}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function createMessage(userObject, serialNumber) {
+  const { firstName, email, pickUpLocation, pickUpDateTime, reservationID } = userObject;
+
+  // Create message object
+  const msg = {
+    to: email,
+    from: { email: 'europauto2021@outlook.com', name: 'Europauto Checkin' },
+    templateId: 'd-d13520409a12422783f1f2bf35983b45',
+    dynamicTemplateData: {
+      firstName: firstName,
+      pickUpLocation: pickUpLocation,
+      pickUpDateTime: getDate(pickUpDateTime),
+      serialNumber: serialNumber,
+      reservationID: reservationID,
+    },
+  };
+
+  return msg;
+}
+
+function sendEmail(message) {
+  // Send message via Sendgrid
+  sgMail
+    .send(message)
+    .then(() => {
+      return 'succes';
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
 }
 
 async function postCreateCheckin(req, res) {
-  // Handle client-side verification
-  const { firstName, email, pickUpLocation, pickUpDateTime, reservationID } = req.body;
-
-  async function postData(url, data) {
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'same-origin',
-      headers: {
-        Authorization: `Basic ${process.env.WALLET_SECRET}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    return response.json();
-  }
-
   postData(process.env.WALLET_URL, req.body).then((data) => {
     if (!data.errors) {
-      console.log(getDate(pickUpDateTime));
-      const msg = {
-        to: email,
-        from: { email: 'europauto2021@outlook.com', name: 'Europauto Checkin' },
-        templateId: 'd-d13520409a12422783f1f2bf35983b45',
-        dynamicTemplateData: {
-          firstName: firstName,
-          pickUpLocation: pickUpLocation,
-          pickUpDateTime: getDate(pickUpDateTime),
-          serialNumber: data.serialNumber,
-          reservationID: reservationID,
-        },
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log('Email sent');
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-
+      // Create message
+      const message = createMessage(req.body, data.serialNumber);
+      // Send e-mail with Sendgrid
+      sendEmail(message);
+      // Send status
       res.send({
         status: '200',
         serialNumber: data.serialNumber,
@@ -197,6 +242,61 @@ async function postCreateCheckin(req, res) {
     }
   });
 }
+
+// async function postCreateCheckin(req, res) {
+//   // Handle client-side verification
+//   const { firstName, email, pickUpLocation, pickUpDateTime, reservationID } = req.body;
+
+//   async function postData(url, data) {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       mode: 'cors',
+//       credentials: 'same-origin',
+//       headers: {
+//         Authorization: `Basic ${process.env.WALLET_SECRET}`,
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
+//     return response.json();
+//   }
+
+//   postData(process.env.WALLET_URL, req.body).then((data) => {
+//     if (!data.errors) {
+//       console.log(getDate(pickUpDateTime));
+//       const msg = {
+//         to: email,
+//         from: { email: 'europauto2021@outlook.com', name: 'Europauto Checkin' },
+//         templateId: 'd-d13520409a12422783f1f2bf35983b45',
+//         dynamicTemplateData: {
+//           firstName: firstName,
+//           pickUpLocation: pickUpLocation,
+//           pickUpDateTime: getDate(pickUpDateTime),
+//           serialNumber: data.serialNumber,
+//           reservationID: reservationID,
+//         },
+//       };
+//       sgMail
+//         .send(msg)
+//         .then(() => {
+//           console.log('Email sent');
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+
+//       res.send({
+//         status: '200',
+//         serialNumber: data.serialNumber,
+//       });
+//     } else {
+//       res.send({
+//         status: '404',
+//         errors: data.errors,
+//       });
+//     }
+//   });
+// }
 
 async function postCreateVerificationSession(req, res) {
   // Post verification to client
@@ -235,22 +335,29 @@ async function postCreatePaymentIntent(req, res) {
   });
 }
 
-function getDate(date) {
-  // Function to calculate specific dates
-  const dateTime = date.split(' ');
-  const dateElements = dateTime[0].split('-');
-  const newDate = `${dateElements[2]}-${dateElements[1]}-${dateElements[0]} ${dateTime[1]}`;
-  const dateObject = new Date(newDate);
-  const day = dateObject.toLocaleString('nl-NL', { day: 'numeric' });
-  const month = dateObject.toLocaleString('nl-NL', { month: 'long' });
-  const year = dateObject.toLocaleString('nl-NL', { year: 'numeric' });
-  const time = dateObject.toLocaleString('nl-NL', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  });
+// function getDate(date) {
+//   // Function to calculate specific dates
+//   const dateTime = date.split(' ');
+//   const dateElements = dateTime[0].split('-');
+//   const newDate = `${dateElements[2]}-${dateElements[1]}-${dateElements[0]} ${dateTime[1]}`;
+//   const dateObject = new Date(newDate);
+//   const day = dateObject.toLocaleString('nl-NL', { day: 'numeric' });
+//   const month = dateObject.toLocaleString('nl-NL', { month: 'long' });
+//   const year = dateObject.toLocaleString('nl-NL', { year: 'numeric' });
+//   const time = dateObject.toLocaleString('nl-NL', {
+//     hour: '2-digit',
+//     minute: '2-digit',
+//     hour12: false,
+//   });
 
-  return `${day} ${month} ${year} om ${time} uur`;
+//   return `${day} ${month} ${year} om ${time} uur`;
+// }
+
+function getDate(date) {
+  moment.locale('nl');
+  const momentDate = moment(date, 'DD-MM-YYYY hh:mm');
+
+  return `${momentDate.format('D MMMM YYYY')} om ${momentDate.format('hh:mm')} uur`;
 }
 
 // //////////////////////////////////////
@@ -258,11 +365,3 @@ function getDate(date) {
 // //////////////////////////////////////
 
 exports.app = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
